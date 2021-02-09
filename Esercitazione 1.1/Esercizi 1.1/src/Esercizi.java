7ecdd8d6e225ca2f2eab680f5ade3fc5f9acf9f1
Pengfei Chen <penfi_chen@hotmail.com>
2021-02-09 18:47:29 +0100

    Esercizi 1 e 1.1 OK

--- Esercitazione 1.1/Esercizi 1.1/src/Esercizi.java
+++ Esercitazione 1.1/Esercizi 1.1/src/Esercizi.java
@@ -5,10 +5,8 @@ public class Esercizi {
  * 	dato un array di interi, popolato casualmente, ordinarlo in ordine crescente
  * 	e stampare a video il risultato di tale ordinamento.
  */
-	public static void ordineCrescente() {
-		
-		int[] interi = {2, 4, 1, 0, 6, -1, 123, 45};
-		
+	public static void ordineCrescente(int[] interi) {
+			
 		for(int i = 0; i < interi.length -1; i++) {
 			int attuale = i;
 			
@@ -24,13 +22,15 @@ public class Esercizi {
 		
 		for(int i =0; i < interi.length; i++)
 			System.out.print(interi[i] + " ");
+		
+		System.out.println();
 
 	}
 
 	
 /*
- * Scrivere un metodo che verifichi se una data stringa inserita in input ¨¨ palindroma,
- * tale metodo dovr¨¤ restituire un booleano.	
+ * 	Scrivere un metodo che verifichi se una data stringa inserita in input ¨¨ palindroma,
+ * 	tale metodo dovr¨¤ restituire un booleano.	
  */
 	public static boolean isPalindroma(String str) {
 		String stringa = "";
@@ -47,8 +47,8 @@ public class Esercizi {
 	
 	
 /*
- * Scrivere un metodo che mostri i primi 50 numeri della serie di fibonacci
- * (i primi due numeri di fibonacci sono 0 e 1 i successivi si calcolano come somma dei 2 precedenti).
+ * 	Scrivere un metodo che mostri i primi 50 numeri della serie di fibonacci
+ * 	(i primi due numeri di fibonacci sono 0 e 1 i successivi si calcolano come somma dei 2 precedenti).
  */
 	public static void fib() {
 		long prec1 = 0;
@@ -65,16 +65,53 @@ public class Esercizi {
 			prec2 = sum;
 		}
 	}
+
+// 	Data una matrice effettuare la trasposta della stessa.
+	public static void isTrasposta(int[][] matrix) {
+		
+		int [][] mTrasp = new int[matrix[0].length][matrix.length];
+		
+		for(int i = 0; i < matrix.length; i++) {
+			for( int j = 0; j < matrix[i].length; j++) {
+				mTrasp[j][i] = matrix[i][j];
+			}
+		}
+		
+		for(int i = 0; i < mTrasp.length; i++) {
+			for(int j = 0; j < mTrasp[i].length; j++)
+				System.out.print(mTrasp[i][j] + " ");
+			
+			System.out.println("");
+		}
+		
+	}
 	
 	
 	public static void main(String[] args) {
+		int[] interi = {2, 4, 1, 0, 6, -1, 123, 45};
 		
-	//	ordineCrescente();
+		int[][] matrix = {
+				{2, 4, 8},
+				{3, 2, 0},
+				{5, 3, 1},
+				{0, 1, 0}
+		};
 		
-	//	boolean t = isPalindroma("alla");
-	//	System.out.print(t);
+		int[][] matrix2 = {
+				{2, 3, 5, 0},
+				{4, 2, 3, 1},
+				{8, 0, 1, 0}
+		};
+	
+		
+		ordineCrescente(interi);
+		
+		boolean t = isPalindroma("alla");
+		System.out.println(t);
 		
 		fib();
+		
+		isTrasposta(matrix);
 
 	}
 }
