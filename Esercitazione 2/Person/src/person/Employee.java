8239b1aa7d3cd7b62fe561a183cba8f11e03790e
Pengfei Chen <penfi_chen@hotmail.com>
2021-02-16 20:34:16 +0100

    Esercitazione 2.2 Employee OK

---
+++ Esercitazione 2/Person/src/person/Employee.java
@@ -0,0 +1,56 @@
+package person;
+
+public class Employee extends Person {
+	private int annoAssunzione;
+	private int stipendio;
+	
+	public Employee(int annoAssunzione, int stipendio) {
+		super();
+		this.annoAssunzione = annoAssunzione;
+		this.stipendio = stipendio;
+	}
+
+	public Employee(String name, String surname, String tax_code, String city, int annoAssunzione, int stipendio) {
+		super(name, surname, tax_code, city);
+		this.annoAssunzione = annoAssunzione;
+		this.stipendio = stipendio;
+	}
+
+	public int getAnnoAssunzione() {
+		return annoAssunzione;
+	}
+
+	public int getStipendio() {
+		return stipendio;
+	}
+
+	public void setAnnoAssunzione(int annoAssunzione) {
+		this.annoAssunzione = annoAssunzione;
+	}
+
+	public void setStipendio(int stipendio) {
+		this.stipendio = stipendio;
+	}
+
+	public void visualize() {
+		System.out.println("Nome: " + super.getName());
+		System.out.println("Cognome: " + super.getSurname());
+		System.out.println("Cod. Fiscale: " + super.getTax_code());
+		System.out.println("Citt¨¤: " + super.getCity());
+		System.out.println("Anno Assunzione: " + this.getAnnoAssunzione());
+		System.out.println("Stipendio: " + this.getStipendio());
+		System.out.println("Anno di Nascita: " + super.bornYear());
+	}
+	
+	public boolean gainsMore(Employee e) {
+		if(e != null) {
+			if(this.getStipendio() > e.getStipendio())
+				return true;
+			else
+				return false;
+		} else 
+			return false;
+	}
+	
+
+}
---
+++ Esercitazione 2/Person/src/person/MainEmployee.java
@@ -0,0 +1,28 @@
+package person;
+
+public class MainEmployee {
+
+	public static void main(String[] args) {
+		Employee[] emp = new Employee[3];
+		
+		emp[0] = new Employee("Mario","Rossi","MARROS90B19Z210Q","Milano",2003,1002);
+		emp[1] = new Employee("Luigi","Blues","LUIBLU93C23Z210Q","Milano",2010,1001);
+		emp[2] = new Employee("Lucas","Verdi","LUCVER92B19Z210Q","Milano",2019,1003);
+		
+		
+		int indMax = 0;
+		for(int i = 0; i < emp.length; i++)
+			if(!emp[indMax].gainsMore(emp[i]))
+				indMax = i;
+		
+		System.out.println("Nome: " + emp[indMax].getName());
+		System.out.println("Cognome: " + emp[indMax].getSurname());
+		System.out.println("Cod. Fiscale: " + emp[indMax].getTax_code());
+		System.out.println("Citt¨¤: " + emp[indMax].getCity());
+		System.out.println("Anno Assunzione: " + emp[indMax].getAnnoAssunzione());
+		System.out.println("Stipendio: " + emp[indMax].getStipendio());
+		System.out.println("Anno di Nascita: " + emp[indMax].bornYear());
+		
+	}
+
+}
